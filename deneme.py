b1e71e6791c3ca3dfbe4925308230b0e5705dbb6
Burak Taha Başda <155187324+BasdaBT@users.noreply.github.com>
2025-09-27 13:59:24 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,5 +1,7 @@
 import cv2
+import numpy as np
 
+import imgback
 
 img = cv2.imread(r'C:\work_work\elektrik.jpeg', cv2.IMREAD_GRAYSCALE)
 cv2.imwrite(r'C:\work_work\elektrik2.jpeg', img)
@@ -15,6 +17,20 @@ cv2.imshow("Gri Resim 2", img_gray)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+threshold = 127
+height,width = img_gray.shape
+img_bw = np.zeros((height,width), dtype=np.uint8)
+for y in range(height):
+    for x in range(width):
+        if img_gray[y][x] >= threshold:
+            img_bw[y][x] = 255
+        else:
+            img_bw[y][x] = 0
+
+cv2.imwrite(r'C:\work_work\elektrik5.jpeg', img_bw)
+cv2.imshow("Siyah Beyaz Resim", img_bw)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 img_rgb = cv2.cvtColor(img_color, cv2.COLOR_BGR2RGB)
 cv2.imwrite(r'C:\work_work\elektrik4.jpeg', img_rgb)
